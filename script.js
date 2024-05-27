aca844b2844b5481e19dd674bd176b9aed68fcbb
adrielmedina <156657041+adrielmedina@users.noreply.github.com>
2024-05-27 11:22:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,304 @@
+
+const data = [
+    {
+        "CIF": "B0231535372",
+        "NOMBRE": "HERNANDEZ Y ATENCIA SL",
+        "CUPS": "ES002100000358263FN",
+        "TARIFA": "6.1TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "SE LE HA HECHO CAMBIO DE POTENCIA",
+        "FECHA": "15-Nov"
+    },
+    {
+        "CIF": "B16993131",
+        "NOMBRE": "TE COMPRO Y PUNTO SL",
+        "CUPS": "ES0021000004888369LS",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "FIRMADO",
+        "FECHA": "21-Nov",
+        "PAGADO": 60,
+        "O 50%": 30
+    },
+    {
+        "CIF": "52760917K",
+        "NOMBRE": "MARIA DOLORES DOMINGUEZ",
+        "CUPS": "ES0021000000394104RL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 35
+    },
+    {
+        "CIF": "52760917K",
+        "NOMBRE": "MARIA DOLORES DOMINGUEZ",
+        "CUPS": "ES0220040039750113KK",
+        "TARIFA": "RL1",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "FRAN VILLAROBLEDO",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 25
+    },
+    {
+        "CIF": "05247710F",
+        "NOMBRE": "ROCIO GONZALEZ DOS SANTOS",
+        "CUPS": "ES0315000011413712MD",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "CHUS",
+        "ESTADO": "TRAMITADO",
+        "FECHA": "15-Nov",
+        "PAGADO": 35
+    },
+    {
+        "CIF": "04854540E",
+        "NOMBRE": "RAUL BARDERA MARTIN",
+        "CUPS": "ES0021000001813732SH",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO"
+    },
+    {
+        "CIF": "B04979373",
+        "NOMBRE": "THINK ZERO SL",
+        "CUPS": "ES0021000019532063QT",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B88443080",
+        "NOMBRE": "BE BRAVE SL",
+        "CUPS": "ES0031104407910004DN",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "TARIFA": "20TD",
+        "COMERCIALIZADORA": "GANA ENERGIA",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "ACCIONES": "PAGADO"
+    },
+    {
+        "CIF": "B87622195",
+        "NOMBRE": "CAVEREL ASSETS SL",
+        "CUPS": "ES0021000011732140TK",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 525.87,
+        "O 50%": 262.93
+    },
+    {
+        "CIF": "B82419335",
+        "NOMBRE": "ROBLESFER SL",
+        "CUPS": "ES0021000022413423BN",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 654.31,
+        "O 50%": 327.15
+    },
+    {
+        "CIF": "B88134615",
+        "NOMBRE": "ROCKETFOOD SL",
+        "CUPS": "ES0021000039244351TT",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 503.73,
+        "O 50%": 251.86
+    },
+    {
+        "CIF": "B80149986",
+        "NOMBRE": "CASTELLANA GOLF ",
+        "CUPS": "ES0021000020399272TV",
+        "TARIFA": "30TD",
+        "COMERCIALIZADORA": "EVOLVE",
+        "COMERCIAL": "LUBO",
+        "ESTADO": "TRAMITADO",
+        "PAGADO": 403.35,
+        "O 50%": 201.68
+    }
+];
+
+
+const container = document.getElementById('data-container');
+
+function renderData(data) {
+    container.innerHTML = '';
+    data.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.classList.add('data-item');
+        itemElement.innerHTML = `
+            <p><strong>CIF:</strong> ${item.CIF}</p>
+            <p><strong>Nombre:</strong> ${item.NOMBRE}</p>
+            <p><strong>CUPS:</strong> ${item.CUPS}</p>
+            <p><strong>Tarifa:</strong> ${item.TARIFA}</p>
+            <p><strong>Comercializadora:</strong> ${item.COMERCIALIZADORA}</p>
+            <p><strong>Comercial:</strong> ${item.COMERCIAL}</p>
+            <p><strong>Estado:</strong> ${item.ESTADO}</p>
+            <p><strong>Acciones:</strong> ${item.ACCIONES}</p>
+            <p><strong>Fecha:</strong> ${item.FECHA}</p>
+            <p><strong>Pagado:</strong> ${item.PAGADO || 'N/A'}</p>
+            <p><strong>50%:</strong> ${item["O 50%"] || 'N/A'}</p>
+        `;
+        container.appendChild(itemElement);
+    });
+}
+
+
+function searchData() {
+    const cif = document.getElementById('search-cif').value.toLowerCase();
+    const nombre = document.getElementById('search-nombre').value.toLowerCase();
+    const comercializadora = document.getElementById('search-comercializadora').value.toLowerCase();
+    const estado = document.getElementById('search-estado').value.toLowerCase();
+
+    const filteredData = data.filter(item => {
+        return (
+            (!cif || item.CIF.toLowerCase().includes(cif)) &&
+            (!nombre || item.NOMBRE.toLowerCase().includes(nombre)) &&
+            (!comercializadora || item.COMERCIALIZADORA.toLowerCase().includes(comercializadora)) &&
+            (!estado || item.ESTADO.toLowerCase().includes(estado))
+        );
+    });
+
+    renderData(filteredData);
+}
+
+
+renderData(data);
+
+function generateChart() {
+    
+    const actionsData = data.reduce((acc, curr) => {
+        const action = curr.ACCIONES || 'Undefined';
+        acc[action] = (acc[action] || 0) + 1;
+        return acc;
+    }, {});
+
+    const actionsLabels = Object.keys(actionsData);
+    const actionsValues = Object.values(actionsData);
+
+    
+    const surveyData = {
+        labels: actionsLabels,
+        datasets: [{
+            label: 'Acciones',
+            data: actionsValues,
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                
+            ],
+            hoverOffset: 4
+        }]
+    };
+
+    
+    const surveyConfig = {
+        type: 'doughnut',
+        data: surveyData,
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Acciones',
+                    fontSize: 18
+                },
+                legend: {
+                    display: true,
+                    position: 'bottom',
+                    labels: {
+                        font: {
+                            size: 14
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    
+    const surveyCtx = document.getElementById('survey-chart').getContext('2d');
+    
+    new Chart(surveyCtx, surveyConfig);
+}
+
+
+window.onload = generateChart;
